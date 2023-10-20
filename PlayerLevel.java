222f968f2a91d15dfcbac1da871c9cfa5dc4db5e
2022FA <115386395+2022FA@users.noreply.github.com>
2023-10-20 17:59:05 -0400

    Update PlayerLevel.java
    maximum/minimum level

--- PlayerLevel.java
+++ PlayerLevel.java
@@ -3,30 +3,34 @@ import java.util.Scanner;
 class LevelUp
 {
     private String name;
-    private String level;
+    private int level;
     private int HP;
     public void Character(String name, int level, int HP){
         this.name = name;
         this.HP = 12;
     }
-    public void increaseLevel (String level){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Level");
-
-        level =input.nextLine();
+    public void increaseLevel (int level){
+        if (level > 20){
+            level = 20;
+            System.out.println("level cannot go above 20.");}
+        else{
+        level = level + 1;
     System.out.println("Character's Level is now " + level);
     HP = HP + 7;
-    System.out.println("Character's HP is now " + HP);
+    System.out.println("Character's HP is now " + HP);}
     }
 
 
-    public void decreaseLevel(){
-        System.out.println("Enter Level");
-    Scanner input = new Scanner(System.in);
-    level =input.nextLine();
+    public void decreaseLevel( int level){
+        if (level < 1){
+                level = 1;
+                System.out.println("level cannot go below 1.");}
+        else{
+            level = level - 1;
         System.out.println("Character's Level is now " + level);
         HP = HP - 7;
-        System.out.println("Character's HP is now " +HP);}
+        System.out.println("Character's HP is now " + HP);}
+    }
 }
 
 
