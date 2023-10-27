2517080da39de5520111687ee7377d9924fd04b7
HarrisonPegram <143549947+HarrisonPegram@users.noreply.github.com>
2023-10-27 16:33:29 -0400

    basehp
    - added HP based on class input.
    - removed characterMaxHP.

--- Main1.java
+++ Main1.java
@@ -8,30 +8,42 @@ public class Main1 {
         String characterRace;
         int characterAbilityScore;
         int characterArmorClass;
-        int characterMaxHealth;
         int characterProficiencyBonus;
         int characterLevel;
+        int characterBaseHealth = 0;
         String characterClass;
         ArrayList<String> characterInventory;
         ArrayList<String> characterFeatures;
 
         Scanner keyboard = new Scanner(System.in);
+
         party = getStringInput(keyboard, "Enter party name");
         characterName = getStringInput(keyboard, party + " create a character name");
         characterRace = getStringInput(keyboard, "Enter a race for " + characterName);
         characterClass = getStringInput(keyboard, "Enter the class for " + characterName);
 
+        characterBaseHealth = characterBaseHPBasedOnClass(characterClass, keyboard, characterBaseHealth, characterName);
+
         characterAbilityScore = integerValidation(keyboard, "Ability Score", characterName);
         characterArmorClass = integerValidation(keyboard, "Armor Class", characterName);
-        characterMaxHealth = integerValidation(keyboard, "Max Health", characterName);
         characterProficiencyBonus = integerValidation(keyboard, "Proficiency Bonus", characterName);
         characterLevel = integerValidation(keyboard, "Character Level", characterName);
 
         characterInventory = getInventoryInput(keyboard, characterName);
         characterFeatures = getFeaturesInput(keyboard, characterName);
 
-        consoleOutput(party, characterName, characterRace, characterAbilityScore, characterArmorClass, characterMaxHealth, characterProficiencyBonus, characterLevel, characterClass, characterInventory, characterFeatures);
+        consoleOutput(party, characterName, characterRace, characterBaseHealth, characterAbilityScore, characterArmorClass, characterProficiencyBonus, characterLevel, characterClass, characterInventory, characterFeatures);
+
+        keyboard.close();
     }
+
+    private static int characterBaseHPBasedOnClass(String characterClass, Scanner keyboard, int characterBaseHealth, String characterName) {
+        if (characterClass.equalsIgnoreCase("barbarian")) {
+            characterBaseHealth = 12;
+        }
+        return characterBaseHealth;
+    }
+
 
     private static String getStringInput(Scanner keyboard, String prompt) {
         System.out.println(prompt);
@@ -81,13 +93,13 @@ public class Main1 {
         return features;
     }
 
-    private static void consoleOutput(String party, String characterName, String characterRace, int characterAbilityScore, int characterArmorClass, int characterMaxHealth, int characterProficiencyBonus, int characterLevel, String characterClass, ArrayList<String> characterInventory, ArrayList<String> characterFeatures) {
+    private static void consoleOutput(String party, String characterName, String characterRace, int characterBaseHealth, int characterAbilityScore, int characterArmorClass, int characterProficiencyBonus, int characterLevel, String characterClass, ArrayList<String> characterInventory, ArrayList<String> characterFeatures) {
         System.out.println("Party: " + party);
         System.out.println("Character Name: " + characterName);
         System.out.println("Character Race: " + characterRace);
         System.out.println("Ability Score: " + characterAbilityScore);
         System.out.println("Armor Class: " + characterArmorClass);
-        System.out.println("Max Health: " + characterMaxHealth);
+        System.out.println("Base Health: " + characterBaseHealth);
         System.out.println("Proficiency Bonus: " + characterProficiencyBonus);
         System.out.println("Character Level: " + characterLevel);
         System.out.println("Character Class: " + characterClass);
