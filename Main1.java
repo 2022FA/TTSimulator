1c83e5ade3fd67f0724bcfcd5d1d65dfced9e16f
HarrisonPegram <143549947+HarrisonPegram@users.noreply.github.com>
2023-11-03 15:44:00 -0400

    updated
    updated

--- Main1.java
+++ Main1.java
@@ -6,11 +6,11 @@ public class Main1 {
         // Declare variables to store character and party information
         String party;
         String characterName;
-        String characterRace = "";
+        String characterRace;
         int characterAbilityScore;
         int characterArmorClass;
-        int characterProficiencyBonus;
-        int characterBaseLevel= 1;
+        int characterProficiencyBonus = 2;
+        int characterLevel = 1;
         int characterBaseHealth = 0; // Initialize characterBaseHealth
         String characterClass;
         ArrayList<String> characterInventory;
@@ -22,39 +22,88 @@ public class Main1 {
         // Prompt the user for various character and party details
         party = getStringInput(keyboard, "Enter party name");
         characterName = getStringInput(keyboard, party + " create a character name");
-        characterRace = characterRaceValidation(keyboard, characterName, characterRace);
+        characterRace = characterRaceValidation(keyboard, characterName);
 
         characterClass = getStringInput(keyboard, "Enter the class for " + characterName);
 
         // Calculate character's base health based on class
-        characterBaseHealth = characterBaseHPBasedOnClass(characterClass, characterBaseHealth, characterName);
+        characterBaseHealth = characterBaseHPBasedOnClass(characterClass, characterBaseHealth);
 
         // Prompt for and validate other character attributes
         characterAbilityScore = integerValidation(keyboard, "Ability Score", characterName);
         characterArmorClass = integerValidation(keyboard, "Armor Class", characterName);
-        characterProficiencyBonus = integerValidation(keyboard, "Proficiency Bonus", characterName);
 
         // Gather character inventory and features
         characterInventory = getInventoryInput(keyboard, characterName);
         characterFeatures = getFeaturesInput(keyboard, characterName);
 
         // Display the collected character information
-        consoleOutput(party, characterName, characterRace, characterBaseHealth, characterAbilityScore, characterArmorClass, characterProficiencyBonus, characterBaseLevel, characterClass, characterInventory, characterFeatures);
+        consoleOutput(party, characterName, characterRace, characterBaseHealth, characterAbilityScore, characterArmorClass, characterProficiencyBonus, characterLevel, characterClass, characterInventory, characterFeatures);
+
+        boolean continuePlaying = true;
+
+        while (continuePlaying) {
+            // Collect initial character information (same as before)
+
+            characterLevel = changeCharacterInfo(keyboard, characterLevel);
+
+            // Calculate the proficiency bonus based on the character's class and level
+            characterProficiencyBonus = characterProficiencyBonus(characterClass, characterLevel);
+
+            // Display the updated character information
+            consoleOutput(party, characterName, characterRace, characterBaseHealth, characterAbilityScore, characterArmorClass, characterProficiencyBonus, characterLevel, characterClass, characterInventory, characterFeatures);
+
+            // Ask if the user wants to continue
+            System.out.print("Do you want to make more changes (y/n)? ");
+            String continueChoice = keyboard.nextLine().toLowerCase();
+            if (!continueChoice.equals("y")) {
+                continuePlaying = false;
+            }
+        }
 
         // Close the Scanner to release resources
         keyboard.close();
     }
 
-    // Method to calculate the character's base health based on class
-    private static int characterBaseHPBasedOnClass(String characterClass, int characterBaseHealth, String characterName) {
+    private static int characterProficiencyBonus(String characterClass, int characterLevel) {
+        int proficiencyBonus = 0;
+        if (characterClass.equalsIgnoreCase("barbarian")) {
+            proficiencyBonus = barbarianProficiencyBonusBasedOnLevel(characterLevel, proficiencyBonus);
+        }
+        return proficiencyBonus;
+    }
+
+    private static int changeCharacterInfo(Scanner keyboard, int characterLevel) {
+        System.out.print("Do you want to change the level (y/n)? ");
+        String levelChangeChoice = keyboard.nextLine().toLowerCase();
+        if (levelChangeChoice.equals("y")) {
+            System.out.print("Level up (up) or level down (d)? ");
+            String levelChange = keyboard.nextLine().toLowerCase();
+            if (levelChange.equals("up")) {
+                characterLevel++;
+            } else if (levelChange.equals("d")) {
+                characterLevel--;
+            }
+        }
+        return characterLevel;
+    }
+    //Calculate's the character's base health based on class
+    private static int characterBaseHPBasedOnClass(String characterClass, int characterBaseHealth) {
         if (characterClass.equalsIgnoreCase("barbarian")) {
             characterBaseHealth = 12;
         }
         return characterBaseHealth;
     }
 
+    //For class barbarian that assigns a proficiencyBonus based on the character level 2+1/2
+    private static int barbarianProficiencyBonusBasedOnLevel(int characterLevel, int proficiencyBonus) {
+        proficiencyBonus = 2 + ((int) (double) (characterLevel - 1) /4);
+        return proficiencyBonus;
+    }
+
     // Validation loop for character Race.
-    private static String characterRaceValidation(Scanner keyboard, String characterName, String characterRace) {
+    private static String characterRaceValidation(Scanner keyboard, String characterName) {
+        String characterRace;
         do {
             characterRace = getStringInput(keyboard, "Choose a race for " + characterName);
             if (characterRace.equalsIgnoreCase("dragonborn") || characterRace.equalsIgnoreCase("dwarf") || characterRace.equalsIgnoreCase("elf") ||
@@ -69,13 +118,13 @@ public class Main1 {
         return characterRace;
     }
 
-    // Method to get string input from the user
+    //Gets string input from the user
     private static String getStringInput(Scanner keyboard, String prompt) {
         System.out.println(prompt);
         return keyboard.nextLine();
     }
 
-    // Method to validate and get an integer input from the user
+    //Validates and get an integer input from the user
     private static int integerValidation(Scanner keyboard, String inputName, String characterName) {
         int inputValue = 0;
         boolean validInput = false;
@@ -93,7 +142,7 @@ public class Main1 {
         return inputValue;
     }
 
-    // Method to collect character inventory
+    //Collect character inventory
     private static ArrayList<String> getInventoryInput(Scanner keyboard, String characterName) {
         ArrayList<String> inventory = new ArrayList<>();
         System.out.println("Enter the inventory for " + characterName + " (type 'done' when finished):");
@@ -107,7 +156,7 @@ public class Main1 {
         return inventory;
     }
 
-    // Method to collect character features
+    //Collect character features
     private static ArrayList<String> getFeaturesInput(Scanner keyboard, String characterName) {
         ArrayList<String> features = new ArrayList<>();
         System.out.println("Enter the features for " + characterName + " (type 'done' when finished):");
@@ -121,7 +170,7 @@ public class Main1 {
         return features;
     }
 
-    // Method to display character information
+    //Displays character information
     private static void consoleOutput(String party, String characterName, String characterRace, int characterBaseHealth, int characterAbilityScore, int characterArmorClass, int characterProficiencyBonus, int characterLevel, String characterClass, ArrayList<String> characterInventory, ArrayList<String> characterFeatures) {
         System.out.println("Party: " + party);
         System.out.println("Character Name: " + characterName);
